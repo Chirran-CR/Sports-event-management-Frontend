e89bdaab5e0af7fd8aa8ef2ebb06abc9c62d160d
Chirran <swainchittaranjan345@gmail.com>
2023-03-10 11:54:37 +0530

    refresh problem sorted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import store from "./redux/store.js";
+import store,{persistor} from "./redux/store.js";
 import {Provider } from "react-redux";
 import "./index.css";
 // import ContextProvider from './components/participantSelection/components/context/ContextProvider';
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <ContextProvider> */}
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
      <App/>
+     </PersistGate>
     </Provider>
     {/* </ContextProvider> */}
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,20 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./reducer/Rootreducer";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)));
+
+const persistConfig = {
+    key: 'persist-root',
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store=createStore(persistedReducer,composeWithDevTools(applyMiddleware(logger,thunk)));
+
+const persistor = persistStore(store)
 
 export default store;
+export {persistor};
